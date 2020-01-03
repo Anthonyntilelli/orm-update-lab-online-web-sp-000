9a1c41e66757affdbce707418b9e0ad675823706
Anthony Tilelli <anthony@tilelli.me>
2020-01-03 05:05:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,8 @@ class Student
   end
 
   def self.new_from_db(row)
-    Student.new(row[1], row[2],row[0])
+    value = Student.new(row[1], row[2],row[0])
+    binding.pry
   end
   
   def self.find_by_name(name)
